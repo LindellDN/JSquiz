eeed8c924b4f6cdb183368167ab1db577edc49a0
Derek Lindell <dereklindell2@gmail.com>
2022-03-06 03:29:02 -0500

    js comments/ basic html

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//set up start button
+function startQuiz () {
+    let startButton
+
+}
+//set up a timer on start button
+//set up time limit
+//set up incorrect answer removes time from timer
+//set up end-quiz on 0 timer
+//set up end-quiz on all questions answered
+
+//set up score counter
+//set up highscore recorder
+    //initals
+    //high score
+    //save in localstorage
+
+//set up questions
+//set up multiple choice answer choices
+//set up correct answers
+
